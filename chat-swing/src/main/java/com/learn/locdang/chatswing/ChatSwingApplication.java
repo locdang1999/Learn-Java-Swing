d4dd6954fffe60ac8169d5762364507c5530245e
Loc Dang <hahagioi169@gmail.com>
2025-01-04 12:37:38 +0700

    create project chat-swing with Springboot

---
+++ chat-swing/src/main/java/com/learn/locdang/chatswing/ChatSwingApplication.java
@@ -0,0 +1,13 @@
+package com.learn.locdang.chatswing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatSwingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatSwingApplication.class, args);
+	}
+
+}
