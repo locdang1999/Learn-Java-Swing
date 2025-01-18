84f5b419690b5af4c7df04899f88ca90f6d63f30
Loc Dang <hahagioi169@gmail.com>
2025-01-19 00:36:57 +0700

    add WebsocketConfig and WebsocketController

---
+++ chat-swing/src/main/java/com/learn/locdang/chatswing/WebsocketController.java
@@ -0,0 +1,41 @@
+package com.learn.locdang.chatswing;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebsocketController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+    private final WebSocketSessionManager sessionManager;
+
+    @Autowired
+    public WebsocketController(SimpMessagingTemplate messagingTemplate, WebSocketSessionManager sessionManager) {
+        this.messagingTemplate = messagingTemplate;
+        this.sessionManager = sessionManager;
+    }
+
+    @MessageMapping("/message")
+    public void handleMessage(Message message) {
+        System.out.println("Received message from user: " + message.getUser() + ": " + message.getMessage());
+        messagingTemplate.convertAndSend("/topic/messages", message);
+        System.out.println("Send message to /topic/messages: " + message.getUser() + ": " + message.getMessage());
+    }
+
+    @MessageMapping("/connect")
+    public void connectUser(String username) {
+        sessionManager.addUsername(username);
+        sessionManager.broadcastActiveUsername();
+        System.out.println(username + " connected");
+    }
+
+    @MessageMapping("/disconnect")
+    public void disconnectUser(String username) {
+        sessionManager.removeUsername(username);
+        sessionManager.broadcastActiveUsername();
+        System.out.println(username + " disconnected");
+    }
+
+}
